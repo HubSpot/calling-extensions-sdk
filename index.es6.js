1cf24007da66168f351b9cdac6116ba75fd62e1e
Myad Tahajody <myad.tahajody@telavox.se>
2019-07-04 14:33:37 +0200

    Transpile index file as well

---
+++ index.es6.js
@@ -0,0 +1,6 @@
+import CallingExtensions from "./lib/CallingExtensions";
+import * as Constants from "./lib/Constants";
+import IFrameManager from "./lib/IFrameManager";
+
+export default CallingExtensions;
+export { Constants, IFrameManager };
