3ad3eb79ed7d7fddd95e55fe5e1e7084f05bc8af
Harminder Singh <hsingh@hubspot.com>
2019-07-12 15:48:58 -0400

    Add back index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import CallingExtensions from "./src/CallingExtensions";
+import * as Constants from "./src/Constants";
+import IFrameManager from "./src/IFrameManager";
+
+export default CallingExtensions;
+export { Constants, IFrameManager };
