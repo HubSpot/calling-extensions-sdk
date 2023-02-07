209a7f94f8b58a9150d9fabe9b2f23e13261787f
Alonso Cadenas <acadenas@hubspot.com>
2023-02-06 22:17:42 -0600

    Update message

--- demo/scripts/support-node-17.sh
+++ demo/scripts/support-node-17.sh
@@ -7,7 +7,7 @@ if [[ $NODE_VERSION =~ $MATCH ]]; then
     MAX_VERSION=16
     if [ $MAJOR_VERSION -gt $MAX_VERSION ]
     then
-      echo "Node.js v$MAX_VERSION LTS recommended. Setting --openssl-legacy-provider as a workaround."
+      echo "Node.js v$MAX_VERSION LTS recommended. Setting --openssl-legacy-provider to support Node.js v17+."
       npm run support-node-17
     fi
     npm i
