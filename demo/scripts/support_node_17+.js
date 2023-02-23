3504150a9c8061c6f6b4b54012c766d8ccfc8ab3
Alonso Cadenas <acadenas@hubspot.com>
2023-02-23 15:20:04 -0600

    Fix npm run build and add comments

--- demo/scripts/support_node_17+.js
+++ demo/scripts/support_node_17+.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+/**
+ * Uses the openssl legacy provider when using Node.js v17 and higher.
+ * To learn more, visit: https://nodejs.org/es/blog/release/v17.0.0/#openssl-3-0.
+ */
+
 const { spawn } = require('child_process');
 
 const args = process.argv.slice(2);
