2ee4b80dbd9201a99a92fac9ecb9840ae5bda805
Esme Ling <esmelingyr@gmail.com>
2023-04-26 11:34:41 -0700

    Enforce order of events in demo minimal js

--- demos/demo-minimal-js/index.js
+++ demos/demo-minimal-js/index.js
@@ -3,6 +3,90 @@
 import CallingExtensions, { Constants } from "@hubspot/calling-extensions-sdk";
 const { errorType } = Constants;
 
+const state = {
+  isLoggedIn: false,
+  timer: null,
+  callDuration: 0,
+};
+
+function disableButtons(ids) {
+  ids.forEach(id => {
+    document.querySelector(id).setAttribute("disabled", true);
+  });
+}
+
+function enableButtons(ids) {
+  ids.forEach(id => {
+    document.querySelector(id).removeAttribute("disabled");
+  });
+}
+
+function toggleLogin(clickedButtonId) {
+  if (!state.isLoggedIn && (clickedButtonId === "login" || clickedButtonId === "initialize")) {
+    disableButtons(["#login", "#initialize"]);
+    enableButtons(["#logout", "#startcall"]);
+    state.isLoggedIn = true;
+  } else if (state.isLoggedIn && clickedButtonId === "logout") {
+    enableButtons(["#login"]);
+    disableButtons(["#logout", "#startcall", "#answercall", "#endcall", "#savecall"]);
+    state.isLoggedIn = false;
+  }
+}
+
+function startCall() {
+  disableButtons(["#startcall"]);
+  enableButtons(["#answercall", "#endcall"]);
+}
+
+function endCall() {
+  document.querySelector("#answercall").setAttribute("disabled", true);
+  document.querySelector("#endcall").setAttribute("disabled", true);
+  document.querySelector("#savecall").removeAttribute("disabled");
+  document.querySelector("#startCall").removeAttribute("disabled");
+}
+
+function saveCall() {
+  document.querySelector("#savecall").setAttribute("disabled", true);
+  document.querySelector("#startcall").removeAttribute("disabled");
+}
+
+const millisecondsToFormattedDuration = milliseconds => {
+  const seconds = milliseconds / 1000;
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  let minutesString = `${Math.floor(minutes % 60)}`;
+  let secondsString = `${Math.floor(seconds % 60)}`;
+
+  if (secondsString.length === 1) {
+    secondsString = 0 + secondsString;
+  }
+  if (hours > 0 && minutesString.length === 1) {
+    minutesString = 0 + minutesString;
+  }
+
+  const duration = [minutesString, secondsString];
+  if (hours > 0) {
+    duration.unshift(hours.toString());
+  }
+
+  return duration.join(":");
+};
+
+function countTimer() {
+  document.querySelector("#timer").innerHTML = millisecondsToFormattedDuration(state.callDuration + 1000);
+  state.callDuration += 1000;
+}
+
+export function startTimer(callStartTime) {
+  state.callDuration = Date.now() - callStartTime;
+  state.timer = setInterval(countTimer, 1000);
+}
+
+export function clearTimer() {
+  clearInterval(state.timer);
+}
+
 const callback = () => {
   let rowId = 0;
   const incomingMsgContainer = document.querySelector("#incomingMsgs");
@@ -17,10 +101,8 @@ const callback = () => {
 
   const defaultSize = {
     width: 400,
-    height: 600
+    height: 600,
   };
-
-  const state = {};
 
   const cti = new CallingExtensions({
     debugMode: true,
@@ -28,21 +110,13 @@ const callback = () => {
       onReady: () => {
         cti.initialized({
           isLoggedIn: true,
-          sizeInfo: defaultSize
+          sizeInfo: defaultSize,
         });
       },
       onDialNumber: (data, rawEvent) => {
         appendMsg(data, rawEvent);
         const { phoneNumber } = data;
         state.phoneNumber = phoneNumber;
-        window.setTimeout(
-          () =>
-            cti.outgoingCall({
-              createEngagement: true,
-              phoneNumber
-            }),
-          500
-        );
       },
       onEngagementCreated: (data, rawEvent) => {
         const { engagementId } = data;
@@ -56,24 +130,30 @@ const callback = () => {
       },
       onVisibilityChanged: (data, rawEvent) => {
         appendMsg(data, rawEvent);
-      }
-    }
+      },
+    },
   });
 
   const element = document.querySelector(".controls");
   element.addEventListener("click", event => {
+    event.preventDefault();
     const clickedButtonValue = event.target.value;
+    const clickedButtonId = event.target.id;
+
     switch (clickedButtonValue) {
       case "initialized":
         cti.initialized({
-          isLoggedIn: true
+          isLoggedIn: true,
         });
+        toggleLogin(clickedButtonId);
         break;
       case "log in":
         cti.userLoggedIn();
+        toggleLogin(clickedButtonId);
         break;
       case "log out":
         cti.userLoggedOut();
+        toggleLogin(clickedButtonId);
         break;
       // Calls
       case "incoming call":
@@ -81,29 +161,37 @@ const callback = () => {
           cti.incomingCall();
         }, 500);
         break;
-      case "outgoing call started":
+      case "outgoing call started": {
+        const callStartTime = Date.now();
+        startCall();
+        startTimer(callStartTime);
         window.setTimeout(() => {
           cti.outgoingCall({
             createEngagement: "true",
-            phoneNumber: state.phoneNumber
+            phoneNumber: state.phoneNumber,
+            callStartTime,
           });
         }, 500);
         break;
+      }
       case "call answered":
         cti.callAnswered();
         break;
       case "call ended":
+        endCall();
+        clearTimer();
         cti.callEnded();
         break;
       case "call completed":
+        saveCall();
         cti.callCompleted({
-          engagementId: state.engagementId
+          engagementId: state.engagementId,
         });
         break;
       case "send error":
         cti.sendError({
           type: errorType.GENERIC,
-          message: "This is a message shown in Hubspot UI"
+          message: "This is a message shown in Hubspot UI",
         });
         break;
       case "change size":
@@ -111,7 +199,7 @@ const callback = () => {
         defaultSize.height += 20;
         cti.resizeWidget({
           width: defaultSize.width,
-          height: defaultSize.height
+          height: defaultSize.height,
         });
         break;
       default:
@@ -121,8 +209,8 @@ const callback = () => {
 };
 
 if (
-  document.readyState === "interactive" ||
-  document.readyState === "complete"
+  document.readyState === "interactive"
+  || document.readyState === "complete"
 ) {
   window.setTimeout(() => callback(), 1000);
 } else {
