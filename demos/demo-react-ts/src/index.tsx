c2e5c33f9b7e3d2c9ca9657d96a542bfb133962d
Alonso Cadenas <acadenas@hubspot.com>
2023-05-22 16:14:36 -0500

    Revert "test gha"
    This reverts commit ca86ab06b3f4022f945b45521b4d75961047672d.

--- demos/demo-react-ts/src/index.tsx
+++ demos/demo-react-ts/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
-// This is a change
+
 const rootNode = document.getElementById("app");
 if (!rootNode) {
   throw new Error("The element #app wasn't found");
