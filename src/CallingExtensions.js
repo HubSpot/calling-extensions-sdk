e4d5b434021a2bf8313078f3e702e20eb17e58b9
hemang-thakkar <59508898+hemang-thakkar@users.noreply.github.com>
2024-01-03 15:41:03 -0500

    Pass iFrameOptions to IFrameManager inside CallingExtensions constructor (#167)
    * CALL-5597: Pass iFrameOptions to IFrameManager inside CallingExtensions constructor
    
    * 0.2.1-alpha.0
    
    * 0.2.1-beta.0
    
    * 0.2.1

--- src/CallingExtensions.js
+++ src/CallingExtensions.js
@@ -13,7 +13,16 @@ import { messageType, errorType } from "./Constants";
  */
 
 /**
+ * @typedef {Object} IframeOptions
+ * @property {string} src - iframe URL
+ * @property {string} height - Height of iframe
+ * @property {string} width - Width of iframe
+ * @property {string} hostElementSelector - Selector for host element where iframe will be bound
+ */
+
+/**
  * @typedef {Object} Options
+ * @property {IframeOptions} iFrameOptions - iFrame configuration options
  * @property {boolean} debugMode - Whether to log various inbound/outbound debug messages
  * to the console.
  * @property {EventHandlers} eventHandlers - Event handlers handle inbound messages.
@@ -34,6 +43,7 @@ class CallingExtensions {
     this.options = options;
 
     this.iFrameManager = new IFrameManager({
+      iFrameOptions: options.iFrameOptions,
       debugMode: options.debugMode,
       onMessageHandler: event => this.onMessageHandler(event),
     });
